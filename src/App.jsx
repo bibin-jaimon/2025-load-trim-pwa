5990ddc35a63e40f4e55358205fd4e937e03c839
bibin-jaimon <bibinjaimon@gmail.com>
2025-05-17 22:01:47 +0530

    Add json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Table from "./components/Table";
+import { loadAircrafts, loadPressures } from "./data-store";
 
 function App() {
+  const [pressures, setPressures] = useState([]);
+  const [aircrafts, setAircrafts] = useState({});
+
+  useEffect(() => {
+    fetchAircrafts();
+    fetchPressures();
+  }, []);
+
+  const fetchPressures = async () => {
+    const data = await loadPressures();
+    setPressures(data);
+  };
+
+  const fetchAircrafts = async () => {
+    const data = await loadAircrafts();
+    setAircrafts(data);
+  };
+
   return (
     <div className="app-container">
       <h1>Load And Trim</h1>
---
+++ src/data-store/index.js
@@ -0,0 +1,24 @@
+const loadJSONModules = async (modules) => {
+  const imports = Object.values(modules).map((fn) => fn());
+  return await Promise.all(imports);
+};
+
+export const loadAircrafts = async () => {
+  const modules = import.meta.glob("./aircrafts/*.json");
+  let data = await loadJSONModules(modules);
+
+  return mapped(data);
+};
+export const loadPressures = async () => {
+  const modules = import.meta.glob("./pressure/pressure.json");
+  let data = await loadJSONModules(modules);
+  return mapped(data);
+};
+
+const mapped = (data) => {
+  let mapped = data.map((item) => {
+    console.log("mapped", item.default);
+    return item.default;
+  });
+  return mapped;
+};
